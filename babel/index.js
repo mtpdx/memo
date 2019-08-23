7b1c58e77d1625251d4348ef77846d01eb7e20df
sunyh <yinghengsun@deepbaytech.com>
2019-08-23 16:54:18 +0800

    babel

---
+++ babel/index.js
@@ -0,0 +1,9 @@
+console.log('hello babel');
+
+import t , {f, fn} from './test.js'
+
+console.log(t);
+
+t.fn_e()
+f()
+fn()
---
+++ babel/test.js
@@ -0,0 +1,21 @@
+let a = 1
+let b = 3
+let c = '123'
+let d = () => {
+    console.log('hello d');
+
+}
+function fn_e() {
+    console.log('hello fn e');
+
+}
+
+export default {
+    a,
+    b,
+    c,
+    fn_e
+}
+
+export let f = ()=>{console.log('ffffffff')};
+export function fn(){ console.log("fn") }
